764cb64b704efd35539d117047a4d61bc9609dc5
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-11-24 18:25:17 +0100

    "Finished first touch on HTML + CSS, beginning js"

---
+++ script.js
@@ -0,0 +1,6 @@
+// FIXA
+// när jag klickar på start-buttons (addeventlistener)
+// så vill jag dölja elementet start-game-btn-container 
+// och visa image + answers+quit-buttons setAttribute('display=none/block')
+
+const guessAllBtn = document.querySelector('#')
