d4e4a374792c45870993273e5ac51ece537555eb
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-11-26 14:20:33 +0100

    trying to set up functions for the first img to guess + randomizing position of the names on the buttons

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// I värsta fall om allt skiter sig så får jag göra en 
+// HTML-lista med valda bilder där det finns 4 radiobtns 
+// som val på varje och en submit-knapp längst ner.
+
 // FIXA ✅
 // när jag klickar på start-buttons (addeventlistener)
 // så vill jag dölja elementet start-game-btn-container 
@@ -10,8 +14,11 @@ const guessTwentyBtn = document.querySelector('#btn-guess-twenty');
 const startGameContainer = document.querySelector('#start-game-container');
 const studentImage = document.querySelector('#student-image');
 const answBtns = document.querySelectorAll('.answer-btns');
-const quitBtn = document.querySelector('#quit-btn')
-
+const quitBtn = document.querySelector('#quit-btn');
+const answ1Btn = document.querySelector('#answ1');
+const answ2Btn = document.querySelector('#answ2');
+const answ3Btn = document.querySelector('#answ3');
+const answ4Btn = document.querySelector('#answ4');
 
 const showBtnsWhenStart = () => {
     //gör en funktion här som jag kan kalla på för varje startklick (all, 10,20 val)
@@ -24,7 +31,7 @@ const showBtnsWhenStart = () => {
     quitBtn.style = 'display: flex';
 }
 
-const shuffleStudents = (array) => {
+const shuffleArray = (array) => {
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
 		const temp = array[i];
@@ -34,11 +41,13 @@ const shuffleStudents = (array) => {
 }
 
 //cloning the students array into a new array
-let allStudents = [...students]
+let allStudents = [...students];
 
-shuffleStudents(allStudents);
+shuffleArray(allStudents);
 
 let chosenStudents;
+
+const answBtnArray = [answ1Btn, answ2Btn, answ3Btn, answ4Btn];
 
 // console.log(allStudents);
 
@@ -49,53 +58,78 @@ let chosenStudents;
 
 //-------CLICK BUTTONS IN THE BEGINNING AND CHOOSE HOW MANY STUDENTS TO GUESS ON------
 
+const setUpGuess = () => {
+    //Setting the first chosen image and starting game
+    studentImage.setAttribute('src', chosenStudents[0].image);
+
+    //randomizing right answer button
+    shuffleArray(answBtnArray);
+    answBtnArray[0].innerHTML = `${chosenStudents[0].name}`;
+
+}
+
 //choose between ALL, 10 or 20 guesses
+const clickToChooseAmountOfStudents = () => {
 
-guessAllBtn.addEventListener('click', (e) => {
-    //välj ALLA studenter ✅ och starta spel
-    chosenStudents = allStudents; 
-
-    console.log(chosenStudents);
-
-    //hide startGameContainer and show studentImage + all game buttons ✅
-   showBtnsWhenStart();
-    console.log('"All" is clicked');
-});
-guessTenBtn.addEventListener('click', (e) => {
-    //slumpa 10 studenter och starta spel
-        // slumpa med Fisher-Yates ✅
-        chosenStudents = allStudents.slice(0,10); //slumpa ut 10 stycken and slice()? out the 10 first? eller borde finnas en annan metod .filter[0-9]? + .map(student.name) för att få ut namnen 
+    guessAllBtn.addEventListener('click', () => {
+        //välj ALLA studenter ✅ och starta spel
+        chosenStudents = allStudents; 
 
         console.log(chosenStudents);
+         //Setting the first chosen image and starting game
+         setUpGuess();
+        //hide startGameContainer and show studentImage + all game buttons ✅
+        showBtnsWhenStart();
+        console.log('"All" is clicked');
+        
+    });
+    guessTenBtn.addEventListener('click', () => {
+        //slumpa 10 studenter och starta spel
+            // slumpa med Fisher-Yates ✅
+        chosenStudents = allStudents.slice(0,10); //slumpa ut 10 stycken and slice()? out the 10 first? eller borde finnas en annan metod .filter[0-9]? + .map(student.name) för att få ut namnen 
 
-    //hide startGameContainer and show studentImage + all game buttons
-   showBtnsWhenStart();
-   console.log('"10" is clicked');
-});
-guessTwentyBtn.addEventListener('click', (e) => {
+        // console.log(chosenStudents);
+        //Setting the first chosen image and starting game
+        setUpGuess();
+        //hide startGameContainer and show studentImage + all game buttons
+        showBtnsWhenStart();
+        console.log('"10" is clicked');
+    });
+    guessTwentyBtn.addEventListener('click', () => {
     //slumpa 20 bilder och starta spel
         // slumpa med Fisher-Yates
         chosenStudents = allStudents.slice(0,20); //slumpa ut 10 stycken
 
         // console.log(chosenStudents);
-    //hide startGameContainer and show studentImage + all game buttons
-    showBtnsWhenStart();
-    console.log('"20" is clicked');
-    return () => chosenStudents 
-});
-// chosenStudents = click (); //funkar ej. chosenStudents är fortfarande undefined 🙁
-console.log(chosenStudents); //
+            
+        //Setting the first chosen image and starting game
+        setUpGuess();
+        //hide startGameContainer and show studentImage + all game buttons
+        showBtnsWhenStart();
+        console.log('"20" is clicked');
+        return chosenStudents;
+    });
+}
 
+clickToChooseAmountOfStudents();
+
+if(clickToChooseAmountOfStudents === guessTwentyBtn){
+    chosenStudents = allStudents.slice(0,20);
+}
 
 
 //-------END OF CLICKING BUTTONS IN THE BEGINNING AND CHOOSE HOW MANY STUDENTS TO GUESS ON------
 
 
 // hur kan jag få med det valda värdet ovan ut ur scopet i 
-// variabeln chosenStudents och använda det?
+// variabeln chosenStudents och använda det? 
+// tror jag har det nu iom funktionen. För i konsolen
+// så ser jag mitt valda värde när jag skriver in chosenStudents
+console.log(chosenStudents); // hinner ej klicka på knapparna 
+// innan denna skrivs ut i konsolen som undefined
 
 // if(chosenStudents == allStudents.slice(0,20)){
-//     studentImage.setAttribute('src', chosenStudents[0].image);
+
 //     console.log(allStudents);
 // }
 
@@ -122,7 +156,7 @@ let totalGuesses;
 // vill ju inte få ut alla på samma gång. bara en i taget
 
 // chosenStudents.forEach( student => {
-//     studentImage.setAttribute('src', student.image[i])
+    // studentImage.setAttribute('src', chosenStudents.image[1])
 // }); funkar ej
 
 // students.map(student=> student.name); 
