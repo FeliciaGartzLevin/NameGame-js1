5fbdd21a591d16cdffebc0f11209217634b6392f
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-12-01 23:08:23 +0100

    adjusting button paddings and connecting 4 radiobuttons to each picture

--- plan B/testscript.js
+++ plan B/testscript.js
@@ -1,30 +1,31 @@
 document.querySelector('#student-images')
 
-// replace large-wrapper.innerHTML content with this after choosing amount of guesses:
+// replace large-wrapper.innerHTML content with this 
+// (forEach(student)) after choosing amount of guesses:
 /*  
 `   
-<div class="student-images m-4">
+ <div class="student-card m-4">
     <img src="https://placekitten.com/300/300" alt="Student in FED22M" >
     <div id="guess-template" class="m-4 guess-template">
         <!-- alt 1 -->
         <div id="altOne" class="m-2 altOne">
-            <input type="radio" name="${student.name}" value="${student.name}">
-            <label for="${student.name}">Answer 1</label>
+            <input type="radio" name="${guessNumberX}" value="${student.name}">
+            <label for="${guessNumberX}">Answer 1</label>
         </div>
         <!-- alt 2 -->
         <div id="altTwo" class="m-2 altTwo">
-            <input type="radio" name="${student.name}" value="">
-            <label for="${student.name}">Answer 2</label>
+            <input type="radio" name="${guessNumberX}" value="${fromAllStudents.name}">
+            <label for="${guessNumberX}">Answer 2</label>
         </div>
         <!-- alt 3 -->
         <div id="altThree" class="m-2 altThree">
-            <input type="radio" name="${student.name}" value="">
-            <label for="${student.name}">Answer 3</label>
+            <input type="radio" name="${guessNumberX}" value="${fromAllStudents.name}">
+            <label for="${guessNumberX}">Answer 3</label>
         </div>
         <!-- alt 4 -->
         <div id="altFour" class="m-2 altFour">
-            <input type="radio" name="${student.name}" value="">
-            <label for="${student.name}">Answer 4</label>
+            <input type="radio" name="${guessNumberX}" value="${fromAllStudents.name}">
+            <label for="${guessNumberX}">Answer 4</label>
         </div>
     </div>
 </div>
