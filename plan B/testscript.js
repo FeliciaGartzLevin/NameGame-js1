4d1758d5b39d77905d30a2c750222993eb71a212
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-12-01 22:30:27 +0100

    created one single form and added buttons to it for Submit and Quit game

--- plan B/testscript.js
+++ plan B/testscript.js
@@ -1,17 +1,31 @@
 document.querySelector('#student-images')
 
-// replace guessForm.innerHTML with this after choosing amount of guesses:
+// replace large-wrapper.innerHTML content with this after choosing amount of guesses:
 /*  
-`<!-- alt 1 -->          
-    <input type="radio" name="${student.name}" value="${student.name}">
-    <label for="${student.name}">Answer 1</label>
-<!-- alt 2 -->
-    <input type="radio" name="${student.name}" value="">
-    <label for="${student.name}">Answer 2</label>
-<!-- alt 3 -->
-    <input type="radio" name="${student.name}" value="">
-    <label for="${student.name}">Answer 3</label>
-<!-- alt 4 -->
-   <input type="radio" name="${student.name}" value="">
-    <label for="${student.name}">Answer 4</label>
+`   
+<div class="student-images m-4">
+    <img src="https://placekitten.com/300/300" alt="Student in FED22M" >
+    <div id="guess-template" class="m-4 guess-template">
+        <!-- alt 1 -->
+        <div id="altOne" class="m-2 altOne">
+            <input type="radio" name="${student.name}" value="${student.name}">
+            <label for="${student.name}">Answer 1</label>
+        </div>
+        <!-- alt 2 -->
+        <div id="altTwo" class="m-2 altTwo">
+            <input type="radio" name="${student.name}" value="">
+            <label for="${student.name}">Answer 2</label>
+        </div>
+        <!-- alt 3 -->
+        <div id="altThree" class="m-2 altThree">
+            <input type="radio" name="${student.name}" value="">
+            <label for="${student.name}">Answer 3</label>
+        </div>
+        <!-- alt 4 -->
+        <div id="altFour" class="m-2 altFour">
+            <input type="radio" name="${student.name}" value="">
+            <label for="${student.name}">Answer 4</label>
+        </div>
+    </div>
+</div>
 `*/
