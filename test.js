ae7976bf40bae90d0db66e95ff3616d1ea4e82ad
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-12-01 15:56:11 +0100

    created a test.js-file to try redo some of the work and get some new ideas

---
+++ test.js
@@ -0,0 +1,32 @@
+
+// query the guess-buttons
+const guessAllBtn = document.querySelector('#btn-guess-all');
+const guessTenBtn = document.querySelector('#btn-guess-ten');
+const guessTwentyBtn = document.querySelector('#btn-guess-twenty');
+const startGameContainer = document.querySelector('#start-game-container');
+const studentImage = document.querySelector('#student-image');
+const answBtns = document.querySelectorAll('.answer-btns');
+const quitBtn = document.querySelector('#quit-btn');
+const answ1Btn = document.querySelector('#answ1');
+const answ2Btn = document.querySelector('#answ2');
+const answ3Btn = document.querySelector('#answ3');
+const answ4Btn = document.querySelector('#answ4');
+const answContainer = document.querySelector('.answers-container');
+
+
+const shuffleArray = (array) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
+
+
+// replace content in element instead of hiding and showing?
+
+// use filter and map
+
+// tilldela boolean med true/false om namn är gissat eller inte
+// filtrera ut 
