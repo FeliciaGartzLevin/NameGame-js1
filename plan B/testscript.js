b9195e34b3f9c8e64831d8382a8e8aa6b17f7d1f
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-12-01 20:31:30 +0100

    created a skeleton for plan B. trying to make it responsive by grid-ing it, either w/ bootstrap or pure css

---
+++ plan B/testscript.js
@@ -0,0 +1,17 @@
+document.querySelector('#student-images')
+
+// replace guessForm.innerHTML with this after choosing amount of guesses:
+/*  
+`<!-- alt 1 -->          
+    <input type="radio" name="${student.name}" value="${student.name}">
+    <label for="${student.name}">Answer 1</label>
+<!-- alt 2 -->
+    <input type="radio" name="${student.name}" value="">
+    <label for="${student.name}">Answer 2</label>
+<!-- alt 3 -->
+    <input type="radio" name="${student.name}" value="">
+    <label for="${student.name}">Answer 3</label>
+<!-- alt 4 -->
+   <input type="radio" name="${student.name}" value="">
+    <label for="${student.name}">Answer 4</label>
+`*/
